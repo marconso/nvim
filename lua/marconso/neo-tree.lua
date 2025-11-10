bfc8f0e03ebc92980cf10ec26894c39c84cc0d0f
marconso <marconsodehsfilho@gmail.com>
2025-11-10 16:04:56 -0400

    update neotree

--- lua/marconso/neo-tree.lua
+++ lua/marconso/neo-tree.lua
@@ -2,7 +2,10 @@ pcall(function()
   require('neo-tree').setup({
     close_if_last_window = true,
     filesystem = {
-      follow_current_file = true,
+        follow_current_file = {
+            enabled = true,
+            leave_dirs_open = false
+      },
       hijack_netrw_behavior = "open_default",
     },
     window = {
