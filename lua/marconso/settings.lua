411ffa0b7ed1a8bc9be38a57249d1a6bd658db30
marconso <marconsodehsfilho@gmail.com>
2026-03-14 14:36:23 -0400

    troca a chamada do tema para a forma correta

--- lua/marconso/settings.lua
+++ lua/marconso/settings.lua
@@ -11,7 +11,7 @@ opt.winborder = 'rounded'
 opt.updatetime = 200
 opt.termguicolors = true
 opt.scrolloff = 10
-pcall(vim.cmd, "colorscheme catppuccin-macchiato")
+vim.cmd[[colorscheme habamax]]
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
