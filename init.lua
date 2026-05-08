44040ab11cbc907bec36ea3d1448e50b9358fc26
marconso <marconsodehsfilho@gmail.com>
2026-05-08 12:06:06 -0400

    formatacao

--- init.lua
+++ init.lua
@@ -1,138 +1,146 @@
 vim.g.mapleader = " "
-vim.o.tabstop = 4
-vim.o.winborder = "rounded"
-vim.o.rnu = true
-vim.o.wrap = false
-vim.o.signcolumn = "yes"
-vim.o.termguicolors = true
-vim.o.scrolloff = 10
-vim.o.expandtab = true
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.hlsearch = false
-vim.o.incsearch = true
-vim.o.completeopt = "menu,noinsert,menuone,noselect,preview"
-vim.o.autoindent = true
-vim.o.cursorline = true
-vim.o.textwidth = 79
-vim.o.colorcolumn = "80"
 
+local opt = vim.o
+opt.tabstop = 4
+opt.winborder = "rounded"
+opt.rnu = true
+opt.wrap = false
+opt.signcolumn = "yes"
+opt.termguicolors = true
+opt.scrolloff = 10
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+opt.incsearch = true
+opt.completeopt = "menu,noinsert,menuone,noselect,preview"
+opt.autoindent = true
+opt.cursorline = true
+opt.textwidth = 79
+opt.colorcolumn = "80"
 
 vim.api.nvim_create_autocmd("BufReadPost", {
-    callback = function()
-        local _ = vim.api.nvim_buf_get_mark(0, "^")
-        local last_pos = vim.fn.line("'\"")
-        if last_pos > 0 and last_pos <= vim.fn.line("$") then
-            pcall(vim.api.nvim_win_set_cursor, 0, { last_pos, 0 })
-        end
-    end,
+	callback = function()
+		local _ = vim.api.nvim_buf_get_mark(0, "^")
+		local last_pos = vim.fn.line("'\"")
+		if last_pos > 0 and last_pos <= vim.fn.line("$") then
+			pcall(vim.api.nvim_win_set_cursor, 0, { last_pos, 0 })
+		end
+	end,
 })
 
 vim.pack.add({
-    { src = "https://github.com/folke/tokyonight.nvim" },
-    { src = "https://github.com/marko-cerovac/material.nvim" },
-    { src = "https://github.com/rebelot/kanagawa.nvim" },
-    { src = "https://github.com/rose-pine/neovim" },
-    { src = "https://github.com/navarasu/onedark.nvim" },
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/saghen/blink.cmp" },
-    -- { src = "https://github.com/mrcjkb/rustaceanvim" },
-    { src = "https://github.com/lewis6991/gitsigns.nvim" },
-    { src = "https://github.com/nvim-telescope/telescope.nvim" },
-    -- { src = "https://github.com/windwp/nvim-autopairs" },
-    { src = "https://github.com/nvim-mini/mini.nvim" },
-    { src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
-    { src = "https://github.com/nvim-lualine/lualine.nvim" },
-    { src = "https://github.com/nvim-neo-tree/neo-tree.nvim",        branch = "v3.x" },
-    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
-    { src = "https://github.com/MunifTanjim/nui.nvim" },
-    { src = "https://github.com/folke/todo-comments.nvim" },
-    { src = "https://github.com/nvim-lua/plenary.nvim" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-    { src = "https://github.com/jose-elias-alvarez/null-ls.nvim" },
+	{ src = "https://github.com/folke/tokyonight.nvim" },
+	{ src = "https://github.com/marko-cerovac/material.nvim" },
+	{ src = "https://github.com/rebelot/kanagawa.nvim" },
+	{ src = "https://github.com/rose-pine/neovim" },
+	{ src = "https://github.com/navarasu/onedark.nvim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/saghen/blink.cmp" },
+	-- { src = "https://github.com/mrcjkb/rustaceanvim" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+	-- { src = "https://github.com/windwp/nvim-autopairs" },
+	{ src = "https://github.com/nvim-mini/mini.nvim" },
+	{ src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
+	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	{ src = "https://github.com/nvim-neo-tree/neo-tree.nvim", branch = "v3.x" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/MunifTanjim/nui.nvim" },
+	{ src = "https://github.com/folke/todo-comments.nvim" },
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/jose-elias-alvarez/null-ls.nvim" },
 })
-
 
 vim.g.material_style = "darker"
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "python", "lua", "rust", "go", "cpp", "c" },
-    callback = function()
-        vim.bo.textwidth = 79
-        vim.wo.colorcolumn = "80"
-    end,
+	pattern = { "python", "lua", "rust", "go", "cpp", "c" },
+	callback = function()
+		vim.bo.textwidth = 79
+		vim.wo.colorcolumn = "80"
+	end,
 })
 
 require("neo-tree").setup({
-    close_if_last_window = true,
-    filesystem = {
-        follow_current_file = {
-            enabled = true,
-            leave_dirs_open = false,
-            close_if_last_window = true
-        },
-        hijack_netrw_behavior = "open_default",
-    },
-    window = {
-        position = "left",
-        width = 30,
-    },
+	close_if_last_window = true,
+	filesystem = {
+		follow_current_file = {
+			enabled = true,
+			leave_dirs_open = false,
+			close_if_last_window = true,
+		},
+		hijack_netrw_behavior = "open_default",
+	},
+	window = {
+		position = "left",
+		width = 30,
+	},
 })
 
 require("todo-comments").setup()
 require("ibl").setup()
 require("mini.pairs").setup()
 -- require("nvim-autopairs").setup()
-require("lualine").setup({ theme = 'default' })
+require("lualine").setup({ theme = "default" })
 require("telescope").setup()
 require("mason").setup()
 require("blink.cmp").setup({
-    fuzzy = { implementation = "prefer_rust" },
-    completion = {
-        documentation = { auto_show = true },
-    },
-    sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-    },
-    keymap = {
-        preset = "none",
-        ["<C-space>"] = { "show", "fallback" },
-        ["<C-d>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-        ["<Tab>"] = { "select_next", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
-        ["<CR>"] = { "accept", "fallback" },
-    },
+	fuzzy = { implementation = "prefer_rust" },
+	completion = {
+		documentation = { auto_show = true },
+	},
+	sources = {
+		default = { "lsp", "path", "snippets", "buffer" },
+	},
+	keymap = {
+		preset = "none",
+		["<C-space>"] = { "show", "fallback" },
+		["<C-d>"] = { "scroll_documentation_up", "fallback" },
+		["<C-f>"] = { "scroll_documentation_down", "fallback" },
+		["<Tab>"] = { "select_next", "fallback" },
+		["<S-Tab>"] = { "select_prev", "fallback" },
+		["<CR>"] = { "accept", "fallback" },
+	},
 })
 require("gitsigns").setup({
-    signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        topdelete = { text = "-" },
-        delete = { text = "_" },
-        changedelete = { text = "~" },
-    }
+	signs = {
+		add = { text = "+" },
+		change = { text = "~" },
+		topdelete = { text = "-" },
+		delete = { text = "_" },
+		changedelete = { text = "~" },
+	},
 })
 
-
-local servers = { "lua_ls", "pyright", "ruff",
-    "gopls", "rust_analyzer", "clangd",
-    "bashls", "r_language_server", "yamlls" }
+local servers = {
+	"lua_ls",
+	"pyright",
+	"ruff",
+	"stylua",
+	"gopls",
+	"rust_analyzer",
+	"clangd",
+	"bashls",
+	"r_language_server",
+	"yamlls",
+}
 
 vim.lsp.enable(servers)
 
 -- vim.keymap.set({ "n", "x", "v" }, "<leader>lf", vim.lsp.buf.format)
 vim.keymap.set({ "n", "x", "v" }, "<leader>lf", function()
-    vim.lsp.buf.format({
-        async = true,
-        formatting_options = {
-            tabSize = 4,
-            insertSpaces = true,
-        },
-    })
+	vim.lsp.buf.format({
+		async = true,
+		formatting_options = {
+			tabSize = 4,
+			insertSpaces = true,
+		},
+	})
 end)
 
 vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
@@ -143,60 +151,81 @@ vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>g", ":Telescope live_grep<CR>")
 
-
-vim.cmd 'colorscheme material'
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+vim.cmd("colorscheme material")
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 require("nvim-treesitter").setup({
-    indent = { enable = true },
-    highlight = { enable = true },
+	indent = { enable = true },
+	highlight = { enable = true },
 })
 
 local ts = require("nvim-treesitter")
 local parsers = {
-    "bash", "dockerfile", "elixir", "git_config", "gitcommit", "gitignore",
-    "groovy", "go", "heex", "html", "http",
-    "java", "javascript", "lua", "make", "markdown",
-    "markdown_inline", "python", "regex", "rst", "rust", "ssh_config",
-    "sql", "typst", "toml", "tsx", "typescript", "vim", "vimdoc",
-    "yaml",
+	"bash",
+	"dockerfile",
+	"elixir",
+	"git_config",
+	"gitcommit",
+	"gitignore",
+	"groovy",
+	"go",
+	"heex",
+	"html",
+	"http",
+	"java",
+	"javascript",
+	"lua",
+	"make",
+	"markdown",
+	"markdown_inline",
+	"python",
+	"regex",
+	"rst",
+	"rust",
+	"ssh_config",
+	"sql",
+	"typst",
+	"toml",
+	"tsx",
+	"typescript",
+	"vim",
+	"vimdoc",
+	"yaml",
 }
 
 for _, parser in ipairs(parsers) do
-    ts.install(parser)
+	ts.install(parser)
 end
 
 local patterns = {}
 for _, parser in ipairs(parsers) do
-    local parser_patterns = vim.treesitter.language.get_filetypes(parser)
-    for _, pp in pairs(parser_patterns) do
-        table.insert(patterns, pp)
-    end
+	local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+	for _, pp in pairs(parser_patterns) do
+		table.insert(patterns, pp)
+	end
 end
 vim.treesitter.language.register("groovy", "Jenkinsfile")
 
-
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = patterns,
-    callback = function()
-        vim.treesitter.start()
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-    end,
+	pattern = patterns,
+	callback = function()
+		vim.treesitter.start()
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+	end,
 })
 
 vim.keymap.set("v", "<leader>tt", ":!toilet -w 200 -f term -F border<CR>")
 vim.keymap.set("v", "<leader>'", "c''<Esc>P")
-vim.keymap.set('v', '<leader>"', 'c""<Esc>P')
+vim.keymap.set("v", '<leader>"', 'c""<Esc>P')
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank()
-    end
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 vim.keymap.set("n", "<leader>h", function()
-    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end)
